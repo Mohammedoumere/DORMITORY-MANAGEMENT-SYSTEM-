d88b80e6830e7071aaff925aa00437d348a4a8eb
muazshifa <muazshifa9@gmail.com>
2025-12-31 06:21:59 +0300

    ADD Constants.java

---
+++ Constants.java
@@ -0,0 +1,23 @@
+package com.dormitory.utils;
+
+public class Constants {
+    public static final String[] ETHIOPIAN_REGIONS = {
+        "Addis Ababa", "Afar", "Amhara", "Benishangul-Gumuz",
+        "Dire Dawa", "Gambela", "Harari", "Oromia",
+        "Sidama", "Somali", "Southern Nations", "South West",
+        "Tigray"
+    };
+    
+    public static final String[] COLLEGES = {
+        "Engineering", "Natural Sciences", "Social Sciences",
+        "Business & Economics", "Health Sciences", "Law",
+        "Agriculture", "Veterinary Medicine", "Education"
+    };
+    
+    public static final String[] DEPARTMENTS = {
+        "Computer Science", "Electrical Engineering", "Mechanical Engineering",
+        "Civil Engineering", "Chemistry", "Physics", "Mathematics",
+        "Biology", "Economics", "Management", "Accounting",
+        "Medicine", "Nursing", "Pharmacy", "Law", "Agriculture"
+    };
+}
